949f00be1ec75de66d527e5ea40896e7620a2d0a
lennardneuwirth <lennardneuwirth@lennards-mbp.uconnect.utah.edu>
2017-10-13 23:43:46 -0600

    Comparator Bubble Sort: Updated as the original was actually a selection sort.

--- src/genericbubblesort/GenericBubbleSort.java
+++ src/genericbubblesort/GenericBubbleSort.java
@@ -32,12 +32,15 @@ public class GenericBubbleSort {
     
     public static <E> void bubbleSort(E[] list, Comparator<? super E> comparator){
         E temp;
-        for(int i = 0; i < list.length; i++){
-            for(int x = 1; x < list.length; x++){
-                if(comparator.compare(list[i], list[x]) > 1){
-                    temp = list[x];
-                    list[x] = list[i];
-                    list[i] = temp;
+        boolean needNextPass = true;
+        for(int i = 0; i < list.length && needNextPass; i++){
+            needNextPass = false;
+            for(int x = 1; x < list.length - i; x++){
+                if(comparator.compare(list[x], list[x + 1]) > 1){
+                    temp = list[x + 1];
+                    list[x] = list[x + 1];
+                    list[x + 1] = temp;
+                    needNextPass = false;
                 }
             }
         }
