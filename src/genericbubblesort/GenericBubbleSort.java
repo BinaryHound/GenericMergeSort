16dc6a3b17b94addb922f1f8d0e943743a994413
lennardneuwirth <lennardneuwirth@lennards-mbp.uconnect.utah.edu>
2017-10-13 23:33:45 -0600

    Comparable added to bubble sort.

--- src/genericbubblesort/GenericBubbleSort.java
+++ src/genericbubblesort/GenericBubbleSort.java
@@ -1,5 +1,7 @@
 package genericbubblesort;
 
+import java.util.Comparator;
+
 /**
  *
  * @author lennardneuwirth
@@ -15,12 +17,27 @@ public class GenericBubbleSort {
     }
     
     public static <E extends Comparable<E>> void bubbleSort(E[] list){
-        
+        E temp;
+        //Bubble sorts the list in ascending order.
         for(int i = 0; i < list.length; i++){
-            
-            
-            
+            for(int x = 1; x < list.length; x++){
+                if(list[i].compareTo(list[x]) > 1){
+                    temp = list[x];
+                    list[x] = list[i];
+                    list[i] = temp;
+                } 
+            }
         }
+    }
+    
+    public static <E> void bubbleSort(E[] list, Comparator<? super E> comparator){
+        E temp;
+        for(int i = 0; i < list.length; i++){
+            for(int x = 1; x < list.length; x++){
+                
+            }
+        }
+        
         
     }
     
