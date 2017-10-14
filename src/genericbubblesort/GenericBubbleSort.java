d693867a38aa56ac6d801a5598541bd414fdfb7c
lennardneuwirth <lennardneuwirth@lennards-mbp.uconnect.utah.edu>
2017-10-13 23:23:08 -0600

    First Commit.

---
+++ src/genericbubblesort/GenericBubbleSort.java
@@ -0,0 +1,28 @@
+package genericbubblesort;
+
+/**
+ *
+ * @author lennardneuwirth
+ */
+public class GenericBubbleSort {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        
+    }
+    
+    public static <E extends Comparable<E>> void bubbleSort(E[] list){
+        
+        for(int i = 0; i < list.length; i++){
+            
+            
+            
+        }
+        
+    }
+    
+    
+}
